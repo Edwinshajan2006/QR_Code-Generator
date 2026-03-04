65dae77183389ba2e0ee3daf75a46a11e78f4573
Edwin-NeD <edwinshajan2006@gmail.com>
2026-03-04 19:01:52 +0530

    Refactor generate_qrcode to include filename parameter
    Updated the generate_qrcode function to accept a filename parameter for saving the QR code image. Added user prompts for text and filename input.

--- qr.py
+++ qr.py
@@ -1,16 +1,21 @@
 import qrcode
 
-def generate_qrcode(text):
-    qr=qrcode.QRCode(
-        version =1,
+def generate_qrcode(text, filename):
+    qr = qrcode.QRCode(
+        version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
+
     qr.add_data(text)
     qr.make(fit=True)
-    img=qr.make_image(fill_color="black",back_color="white")
-    img.save("qrimg.png")
 
-url=input("enter your url:")
-generate_qrcode(url)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+    print("QR Code saved as", filename)
+
+data = input("Enter text or URL: ")
+file = input("Enter file name (example: qr.png): ")
+
+generate_qrcode(data, file)
